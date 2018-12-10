28f09a5c2453aea83b7f50da18057535615fe909
Snehal-Reddy <snehalreddyk@gmail.com>
2018-12-10 07:16:31 +0530

    only model left

--- preprocess.py
+++ preprocess.py
@@ -11,9 +11,26 @@ def shortest_dist(filename):
 
 	return sd 
 
+def number(filename):
+	num = open(filename,"r")
+	number = []
+	for line in num:
+		lineSplit=line.split(" ")
+		number.append(lineSplit[0])
+	return number
+
 def obstacles(filename):
 	obs = open(filename,"r")
 	x_centre = []
 	y_centre = []
 	area = []
+	for line in obs:
+		lineSplit=line.split(" ")
+		x_centre.append(lineSplit[0])
+		y_centre.append(lineSplit[1])
+		area.append(lineSplit[2])
+	return x_centre,y_centre,area
 	
+
+preprocess.py
+Displaying training.py.
--- training.py
+++ training.py
@@ -1,6 +1,28 @@
 #import pandas as pd
+import numpy as np
 from sklearn.preprocessing import scale
 from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+import preprocess
 
+number = preprocess.number("number.txt")
+shortest_distance = preprocess.shortest_dist("dist.txt")
+x,y,area = preprocess.obstacles("obst.txt")
+
+for i in range(len(number))-1:
+	number[i+1]=number[i]+number[i+1]
+
+training_set_size = number[len(number)-1]
+
+X_all = np.zeros(shape = (3, training_set_size))
+Y_all = np.zeroes(shape = (1,training_set_size))
+
+image = 0
+
+for i in range(training_set_size):
+	if(i>number[image]):
+		shortest_dist = shortest_dist[image]
+	X_all[0][i] = x[i]
+	X_all[1][i] = y[i]
+	X_all[2][i] = area[i]
