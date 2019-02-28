714a13424f84891774033498dbe9a62cfcfe6945
thebhatman <manjunathbhat9920@gmail.com>
2019-02-28 18:34:08 +0530

    Neural network added with working apf

--- apf.cpp
+++ apf.cpp
@@ -106,6 +106,7 @@ int main(int argc, char** argv)
     string s2 = argv[2];
     string s3 = ".jpg";
 	//Mat a(500, 500, CV_8UC1, Scalar(0));
+	cout<<s0+s1+s2+s3<<endl;
 	Mat a = imread(s0+s1+s2+s3,0);
 	a=bin(a);
 
